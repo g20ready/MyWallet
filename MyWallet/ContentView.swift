c3da4d3833822c00c03c41a3a01e9e6b7af2da3c
Marsel Tzatzo <marsel.tzatzo@vodafone.com>
2021-07-03 10:38:56 +0300

    Add Project

---
+++ MyWallet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyWallet
+//
+//  Created by Tzatzo, Marsel, Vodafone Greece on 3/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyWallet/MyWalletApp.swift
@@ -0,0 +1,18 @@
+//
+//  MyWalletApp.swift
+//  MyWallet
+//
+//  Created by Tzatzo, Marsel, Vodafone Greece on 3/7/21.
+//
+
+import SwiftUI
+import Alamofire
+
+@main
+struct MyWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
